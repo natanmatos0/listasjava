8c61649a01ffa91e671c1731239e393b28c471de
natan <natannatan123na@gmail.com>
2025-10-15 22:34:41 -0300

    add the method "amamentar" for the class "mamifero"

--- zoo/src/Mamifero.java
+++ zoo/src/Mamifero.java
@@ -1,6 +1,11 @@
 public class Mamifero extends Animal{
     public Mamifero(String nome, String especie, int numPatas) {
         super(nome, especie, numPatas);
-        if (nome.equalsIgnoreCase("tratador")
+
+    }
+
+
+    public void amamentar(){
+        System.out.println("O animal " + getNome() + " está amamentando");
     }
 }
